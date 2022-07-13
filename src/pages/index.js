cab99cf657d1ba41718dc41bae3806584123a68f
ganesh nikale <ganesh.nikale@digitas.com>
2022-07-14 01:23:22 +0530

    innit commit

---
+++ src/pages/index.js
@@ -0,0 +1,31 @@
+import * as React from "react"
+import {
+  Nav,
+  Button,
+  Display4,
+  Lead,
+  Container,
+  Row,
+  Col,
+  Card,
+  List,
+  BDiv,
+  BH4,
+  BH5,
+  BSmall,
+  BFooter,
+  BImg,
+  BA
+} from 'bootstrap-4-react';
+
+const IndexPage = () => {
+  return (
+    <Container>
+      <Row>
+        <Col >abc</Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default IndexPage
