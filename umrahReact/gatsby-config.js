f2a1301b7ee9e9365299ea98ad6bb97b71823d57
ganesh nikale <ganesh.nikale@digitas.com>
2022-07-15 01:38:30 +0530

    adding static data json

--- umrahReact/gatsby-config.js
+++ umrahReact/gatsby-config.js
@@ -3,5 +3,24 @@ module.exports = {
     title: `umrahReact`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-sass"]
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-transformer-json`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `./data/`,
+      },
+    },
+  ]
 };
