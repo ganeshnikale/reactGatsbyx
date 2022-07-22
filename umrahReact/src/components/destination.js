ecbabbb3b4bafe718c830d526d9727e5d1b4976e
ganesh nikale <ganesh.nikale@digitas.com>
2022-07-22 16:52:00 +0530

    git ignor updated

--- umrahReact/src/components/destination.js
+++ umrahReact/src/components/destination.js
@@ -2,12 +2,13 @@ import React from "react"
 
 export const Destination = (props) => {
     const data = props.pageContext.slug;
+    console.log(data);
     return ( 
         <div className="row">
             
             {
                 data.duration.map( (x, index) =>
-                <p> {x.packageName}</p>
+                <p key={index}> {x.packageName}</p>
                 )
             }
         
